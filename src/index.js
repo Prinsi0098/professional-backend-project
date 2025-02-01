35a14d3eca320c67e9b18225f142cdc1d2a15dc9
prinsi sasani <priyansi.sasani@gmail.com>
2025-02-01 20:28:23 +0530

    database connectivity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello from Node.js");
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+console.log("okk");
+
+
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB();
+
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Errr:-", error);
+//       throw error
+//     })
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`server is running on port ${process.env.PORT}`)
+//     })
+
+//   } catch (error) {
+//     console.error("error", error)
+//     throw error
+//   }
+// })()
